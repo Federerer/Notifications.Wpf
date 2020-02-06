70fdcf220b786819b156a9e3ad3e4f0bba5767f1
APlatonenkov <Platonenkov87@gmail.com>
2020-02-06 08:54:24 +0300

    update nuget

--- Notifications.Wpf/Properties/AssemblyInfo.cs
+++ Notifications.Wpf/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Notifications.Wpf")]
+[assembly: AssemblyTitle("Notification.Wpf")]
 [assembly: AssemblyDescription("Toast notifications for WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Adrian Gaś")]
+[assembly: AssemblyCompany("Aleksandr Platonenkov")]
 [assembly: AssemblyProduct("Notifications.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
@@ -34,6 +34,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
