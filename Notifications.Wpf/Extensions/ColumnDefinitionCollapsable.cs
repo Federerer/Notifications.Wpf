6acf2f8cd868988dcbb64e7420aa265bba5dd230
Platonenkov <Platonenkov87@gmail.com>
2020-02-28 18:30:51 +0300

    add collapsable row and column

---
+++ Notifications.Wpf/Extensions/ColumnDefinitionCollapsable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Notifications.Wpf.Extensions
+{
+    public class ColumnDefinitionCollapsable : ColumnDefinition
+    {
+        static ColumnDefinitionCollapsable()
+        {
+            WidthProperty.OverrideMetadata(
+                typeof(ColumnDefinitionCollapsable),
+                new FrameworkPropertyMetadata(
+                    new GridLength(1, GridUnitType.Star),
+                    null,
+                    (d, v) => ((ColumnDefinitionCollapsable)d).Visible ? v : new GridLength(0)));
+
+            MinWidthProperty.OverrideMetadata(
+                typeof(ColumnDefinitionCollapsable),
+                new FrameworkPropertyMetadata(0d, null, (d, v) => ((ColumnDefinitionCollapsable)d).Visible ? v : 0d));
+        }
+
+        #region Visible : bool - Видимость
+
+        /// <summary>Видимость</summary>
+        public static readonly DependencyProperty VisibleProperty =
+            DependencyProperty.Register(
+                nameof(Visible),
+                typeof(bool),
+                typeof(ColumnDefinitionCollapsable),
+                new PropertyMetadata(true, OnVisibleChanged));
+
+        private static void OnVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            d.CoerceValue(WidthProperty);
+            d.CoerceValue(MinWidthProperty);
+        }
+
+        /// <summary>Видимость</summary>
+        public bool Visible
+        {
+            get => (bool)GetValue(VisibleProperty);
+            set => SetValue(VisibleProperty, value);
+        }
+
+        #endregion
+    }
+}
---
+++ Notifications.Wpf/Extensions/RowDefinitionCollapsable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Notifications.Wpf.Extensions
+{
+    public class RowDefinitionCollapsable : RowDefinition
+    {
+        static RowDefinitionCollapsable()
+        {
+            HeightProperty.OverrideMetadata(
+                typeof(RowDefinitionCollapsable),
+                new FrameworkPropertyMetadata(
+                    new GridLength(1, GridUnitType.Star),
+                    null,
+                    (d, v) => ((RowDefinitionCollapsable)d).Visible ? v : new GridLength(0)));
+
+
+            MinHeightProperty.OverrideMetadata(
+                typeof(RowDefinitionCollapsable),
+                new FrameworkPropertyMetadata(0d, null, (d, v) => ((RowDefinitionCollapsable)d).Visible ? v : 0d));
+        }
+
+        #region Visible : bool - Видимость
+
+        /// <summary>Видимость</summary>
+        public static readonly DependencyProperty VisibleProperty =
+            DependencyProperty.Register(
+                nameof(Visible),
+                typeof(bool),
+                typeof(RowDefinitionCollapsable),
+                new PropertyMetadata(true, OnVisibleChanged));
+
+        private static void OnVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            d.CoerceValue(HeightProperty);
+            d.CoerceValue(MinHeightProperty);
+        }
+
+        /// <summary>Видимость</summary>
+        public bool Visible
+        {
+            get => (bool)GetValue(VisibleProperty);
+            set => SetValue(VisibleProperty, value);
+        }
+
+        #endregion
+    }
+}
