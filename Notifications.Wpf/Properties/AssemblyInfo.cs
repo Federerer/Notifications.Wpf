f6f63fa704be46ba14bdd68985081aa486adf342
APlatonenkov <platonenkov87@gmail.com>
2020-06-04 22:38:32 +0300

    update version nuget

--- Notifications.Wpf/Properties/AssemblyInfo.cs
+++ Notifications.Wpf/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyVersion("1.0.1.11")]
-[assembly: AssemblyFileVersion("1.0.1.11")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
