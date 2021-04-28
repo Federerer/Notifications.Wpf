d100450f6e53f6cf7e007fa2e92399008531556c
APlatonenkov <platonenkov87@gmail.com>
2021-04-28 15:29:38 +0300

    Migrate to net5, add core support, add multiplatforms

--- Notifications.Wpf/Properties/AssemblyInfo.cs
+++ Notifications.Wpf/Properties/AssemblyInfo.cs
@@ -1,38 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Notification.Wpf")]
-[assembly: AssemblyDescription("Toast notifications for WPF")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Aleksandr Platonenkov")]
-[assembly: AssemblyProduct("Notification.Wpf")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c9a4f46-263d-4c23-b361-f09e14bb109e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
-[assembly: AssemblyVersion("2.0.0.8")]
-[assembly: AssemblyFileVersion("2.0.0.8")]
